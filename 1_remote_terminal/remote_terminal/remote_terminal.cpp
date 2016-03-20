5c73ebcfe685b697007c62a6b62eac683ab2602f
artemrudoj <artem.rudoj@frtk.ru>
2016-03-20 22:56:51 +0300

    add service

--- 1_remote_terminal/remote_terminal/remote_terminal.cpp
+++ 1_remote_terminal/remote_terminal/remote_terminal.cpp
@@ -2,22 +2,73 @@
 //
 
 #include "stdafx.h"
+#include "Service.h"
+#include "Utils.h"
+
+
 
 int run_child_with_redirected_io(int argc, TCHAR *argv[]);
 
 int __cdecl server(void);
 int __cdecl client(char *);
 
-int main(int argc, char *argv[])
+
+void _tmain2(int argc, TCHAR *argv[]);
+//void __cdecl _tmain3(int argc, TCHAR *argv[]);
+
+
+int _tmain(int argc, TCHAR *argv[])
 {
-	if (argc == 2 && strcmp(argv[1], "server")==0 ) {
+
+	TCHAR szCommand[10];
+	TCHAR szSvcName[80];
+	if (argc >= 2)
+		StringCchCopy(szCommand, 10, argv[1]);
+	if (argc == 3)
+		StringCchCopy(szSvcName, 80, argv[2]);
+	
+	if (argc == 2 && lstrcmpi(szCommand, TEXT("server")) == 0) {
 		server();
 	}
-	if (argc == 3 && strcmp(argv[1], "client")==0) {
+	else if (argc == 3 && lstrcmpi(szCommand, TEXT("client")) == 0) {
 		char c_szText[100];
 		//wcstombs(c_szText, argv[2], wcslen(argv[2]) + 1);
-		client(argv[2]);
+		client((char *)argv[2]);
 	}
-    return 0;
+	else if (argc == 2 && lstrcmpi(szCommand, TEXT("install")) == 0)
+	{
+		SvcInstall();
+	}
+	else if (argc == 3 && lstrcmpi(szCommand, TEXT("start")) == 0)
+		DoStartSvc(szSvcName);
+	else if (argc == 3 && lstrcmpi(szCommand, TEXT("dacl")) == 0)
+		DoUpdateSvcDacl(szSvcName);
+	else if (argc == 3 && lstrcmpi(szCommand, TEXT("stop")) == 0)
+		DoStopSvc(szSvcName);
+	else if (argc == 3 && lstrcmpi(szCommand, TEXT("delete")) == 0)
+		DoDeleteSvc(szSvcName);
+	else
+	{
+		_tprintf(TEXT("Unknown command (%s)\n\n"), szCommand);
+		DisplayUsage();
+	}
+
+	// TO_DO: Add any additional services for the process to this table.
+	SERVICE_TABLE_ENTRY DispatchTable[] =
+	{
+		{ SVCNAME, (LPSERVICE_MAIN_FUNCTION)SvcMain },
+		{ NULL, NULL }
+	};
+
+	// This call returns when the service has stopped. 
+	// The process should simply terminate when the call returns.
+
+	if (!StartServiceCtrlDispatcher(DispatchTable))
+	{
+		SvcReportEvent(TEXT("StartServiceCtrlDispatcher"));
+	}
+	getchar();
+	return 0;
 }
+
 
